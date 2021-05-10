fe222d92d92e5ea96bb42f9a4622977cb37aa554
Tiagopb <tiago000@outlook.com>
2021-05-10 00:11:33 -0300

    entrega do App

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import './App.css';
 import styled from 'styled-components';
-//import bg from './src/bg.jpeg'
-
-//import { InputMessage } from './components/inputMessage.js';
-//export default () => <img src={bg.jpeg}/>
 
 const HeaderVerde = styled.div`
   display: flex;
@@ -15,7 +11,7 @@ const HeaderVerde = styled.div`
   background-color: #25D366;
   position:fixed;
   top:0;
-  z-index:3;
+  z-index:4;
   h1{
     font-size:2rem;
     color: white;
@@ -24,9 +20,8 @@ const HeaderVerde = styled.div`
     margin-left:-50px;
     border: none;
   }
-
   button:focus {
-    margin-left:-50px;
+    margin-left:-25px;
     border: none;
     opacity:50%;
   }
@@ -35,7 +30,7 @@ const HeaderVerde = styled.div`
     border-radius:50%;
   }
 `
-const ContainerAreaMensagem = styled.div`
+const AppContainer = styled.div`
   border-radius: 0.5rem;
   display: flex;
   flex-direction: column;
@@ -46,14 +41,15 @@ const ContainerAreaMensagem = styled.div`
   box-shadow: 5px 5px 15px -5px #000000;
   background-color: #D3D3D3;
   background-image: url("https://preview.redd.it/qwd83nc4xxf41.jpg?width=640&crop=smart&auto=webp&s=e82767fdf47158e80604f407ce4938e44afc6c25")
-`
+`;
+
 const AreaMensagem = styled.div`
-background-image: url("https://preview.redd.it/qwd83nc4xxf41.jpg?width=640&crop=smart&auto=webp&s=e82767fdf47158e80604f407ce4938e44afc6c25")
-  background-color:#D3D3D3;
+  background-image: url("https://preview.redd.it/qwd83nc4xxf41.jpg?width=640&crop=smart&auto=webp&s=e82767fdf47158e80604f407ce4938e44afc6c25")
+  background-color: #D3D3D3;
   display: flex;
   justify-content: flex-end;
   flex-direction: column;
-  padding: 1rem;
+  padding: 16px;
   flex: 1;
   max-width: 100%;
   height: 100vh;
@@ -63,40 +59,40 @@ background-image: url("https://preview.redd.it/qwd83nc4xxf41.jpg?width=640&crop=
     padding: 5px;
   }
 `;
-const AreaForm = styled.form`
+
+const textFormulario = styled.form`
   display: grid;
   grid-template-columns: 1fr 3fr 1fr;
   justify-content: center;
   align-items: center;
+ 
   button {
-    cursor: pointer;
-    width: 3rem; 
-    margin-left:3em;  
-    background-color:#89CFF0;
-    border-radius:50%; 
-  }
-  img{
-    width:1rem;
-    height:3rem;
-  }
-`;
+    border:1px solid #25692A;
+    border-radius:4px;
+    display:inline-block;
+    cursor:pointer;
+    font-family:Verdana;
+    font-weight:bold;
+    font-size:13px;
+    padding:6px 10px;
+    text-decoration:none;
+}`;
+
 const InputMensagem = styled.input`
   flex: 1;
   border: 0;
   padding: 1rem;
   border-radius: 0.3rem;
-  margin-left: 0.5rem;
+  margin-left: 2rem;
   outline: none;
-
 `;
 const InputNome = styled.input`
-  width: 6.25rem;
-  border: 0;
+    border: 0;
+    width: 9.50rem;
   padding: 1rem;
-  border-radius: 0.3rem;
+  border-radius: 0.5rem;
   margin-left: 3rem;
   outline: none;
-
 `;
 
 
@@ -106,8 +102,8 @@ class App extends React.Component {
     nome: "",
     mensagemUsuario: ""
   };
-
-  doubleClickDeletar = (msgParaDeletar, index) => {
+  // deleta a mensagem na area de texto
+  ClickDelete = (msgParaDeletar, index) => {
     const listaMensagens = [...this.state.mensagens];
 
     const mensagensFiltradas = listaMensagens.filter((mensagem) => {
@@ -115,7 +111,7 @@ class App extends React.Component {
     });
 
     this.setState({ mensagens: mensagensFiltradas });
-    alert("A mensagem foi deletada!");
+    alert("Mensagem deletada!");
   };
 
   onChangeNome = (event) => {
@@ -125,33 +121,33 @@ class App extends React.Component {
     this.setState({ mensagemUsuario: event.target.value });
   };
 
-  enviarMensagem = (event) => {
+  sendMessage = (event) => {
     event.preventDefault();
     const novaMensagem = {
       nome: this.state.nome,
       mensagem: this.state.mensagemUsuario
     };
-
+// spread do novo array com o atual
     const todasMensagens = [...this.state.mensagens, novaMensagem];
     this.setState({ mensagens: todasMensagens, mensagemUsuario: "" });
   };
 
   render() {
     return (
-      <ContainerAreaMensagem>
+      <AppContainer>
         <HeaderVerde>
-          <button><img src="https://www.flaticon.com/svg/vstatic/svg/271/271220.svg?token=exp=1620586064~hmac=e29f4627fd84b76dd8c203f15de95a1a" 
-          alt="my image" onClick={this.myfunction} /></button>
+          
           <h1>Conversas</h1>
           <img src="https://picsum.photos/50" alt="user"></img>
         </HeaderVerde>
+
         <AreaMensagem>
           {this.state.mensagens.map((mensagem, index) => {
             return (
               <p
                 key={index}
                 onDoubleClick={() =>
-                  this.doubleClickDeletar(mensagem.mensagem, index)
+                  this.ClickDelete(mensagem.mensagem, index)
                 }
               >
                 <strong>{mensagem.nome}</strong>: {mensagem.mensagem}
@@ -159,23 +155,23 @@ class App extends React.Component {
             );
           })}
         </AreaMensagem>
-        <AreaForm onSubmit={this.enviarMensagem}>
+
+        <textFormulario onSubmit={this.sendMessage}>
           <InputNome
             placeholder={"Nome"}
-            value={this.state.nome}
             onChange={this.onChangeNome}
+            value={this.state.nome}
+
           />
           <InputMensagem
             placeholder={"Mensagem"}
             value={this.state.mensagemUsuario}
             onChange={this.onChangeMensagemUsuario}
           />
-          <button><img src="https://www.flaticon.com/svg/vstatic/svg/876/876777.svg?token=exp=1620586676~hmac=a1108d6a1b6580c77950bf3e6f05e0ce" 
-          alt="mymyimage" onClick={this.enviarMensagem}/></button>
-        </AreaForm>
-      </ContainerAreaMensagem>
+          <button onClick={this.sendMessage}>Enviar</button>
+        </textFormulario>
+      </AppContainer>
     );
   }
 }
 export default App;
-
